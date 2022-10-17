7d142e2799b68f962ed229809c2b4d295a0cba5f
Olevova <92223539+Olevova@users.noreply.github.com>
2022-10-17 18:28:45 +0000

    Deploying to gh-pages from @ Olevova/goit-js-hw-09@41ef2767a004f6e912eecbc539febbf1de2993ef 🚀

--- 01-color-switcher.9a22534c.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("body"),o=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");function n(){var o="#".concat(Math.floor(16777215*Math.random()).toString(16));return console.log(o),t.style.backgroundColor=o}t.style.backgroundColor="teal",o.addEventListener("click",(function(){return console.log(15),changeColorId=setInterval(n,1e3)})),e.addEventListener("click",(function(){clearInterval(changeColorId)}))}();
-//# sourceMappingURL=01-color-switcher.9a22534c.js.map
