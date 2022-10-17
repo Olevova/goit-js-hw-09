49e8d12960b63578af76a713ddd9e7f37e1724b8
Olevova <olevova1983@gmail.com>
2022-10-17 21:24:36 +0300

    first

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,18 @@
+const bodyEl = document.querySelector('body');
+const startBtnEl = document.querySelector('button[data-start]');
+const stopBtnEl = document.querySelector('button[data-stop]');
+bodyEl.style.backgroundColor = 'teal';
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function getNewColor() {
+    const color = getRandomHexColor();
+    console.log(color);
+    return  bodyEl.style.backgroundColor = color;
+    
+}
+
+startBtnEl.addEventListener('click', () => {console.log(15); return changeColorId = setInterval(getNewColor, 1000 )})
+stopBtnEl.addEventListener('click', () => {clearInterval(changeColorId)})
