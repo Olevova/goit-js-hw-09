ea72424da09e1b1483142aa2b124569870be2219
Olevova <olevova1983@gmail.com>
2022-10-19 23:25:24 +0300

    second new

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,8 +1,18 @@
 import flatpickr from "flatpickr";
 import 'flatpickr/dist/flatpickr.min.css';
-let dateEnd = null
+import Notiflix from 'notiflix';
+
+let dateEnd = null;
+let timeLeft = null;
 
 const inputEl = document.querySelector("#datetime-picker");
+const timeDays = document.querySelector('.value[data-days]');
+const timeHours = document.querySelector('.value[data-hours]');
+const timeMinutes = document.querySelector('.value[data-minutes]');
+const timeSeconds = document.querySelector('.value[data-seconds]');
+const btnElem = document.querySelector('button');
+
+btnElem.setAttribute("disabled", true);
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -10,9 +20,10 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     if(selectedDates[0] < Date.now()){
-      alert("Please choose right date")
+      Notiflix.Notify.failure("Please choose right date")
       return
     }
+    btnElem.removeAttribute("disabled");
     dateEnd = selectedDates[0];
     console.log(Date.now(dateEnd));
   },
@@ -20,7 +31,40 @@ const options = {
 flatpickr(inputEl, options);
 
 // час від якого потрібно вести відлік
-const timeDays = document.querySelector('.value[data-days]');
-const timeHours = document.querySelector('.value[data-hours]');
-const timeMinutes = document.querySelector('.value[data-minutes]');
-const timeSeconds = document.querySelector('.value[data-seconds]');
+
+btnElem.addEventListener("click", SomeNew)
+function SomeNew() {
+    setInterval(() => {
+    timeLeft = dateEnd - Date.now();
+    // console.log(timeLeft);
+    const { seconds, minutes, hours, days } = convertMs(timeLeft);
+    timeSeconds.textContent = seconds;
+      timeMinutes.textContent = minutes;
+      timeHours.textContent = hours;
+      timeDays.textContent = days;
+  }, 1000);
+  
+
+
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+// console.log(convertMs(timeLeft)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
