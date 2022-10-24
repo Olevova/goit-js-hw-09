db9e8b6b99a0dab10d3fa655f76978a3138b678a
Olevova <92223539+Olevova@users.noreply.github.com>
2022-10-24 16:10:50 +0000

    Deploying to gh-pages from @ Olevova/goit-js-hw-09@db15a070aa9076d9ceb5b9105b5ded209c3cb719 🚀

---
+++ 03-promises.9ba487a7.js
@@ -0,0 +1,2 @@
+const e={delayEl:document.querySelector("input[name='delay']"),stepDelayEl:document.querySelector("input[name='step']"),amountEl:document.querySelector("input[name='amount']"),btnEl:document.querySelector("button")};console.log(e);
+//# sourceMappingURL=03-promises.9ba487a7.js.map
