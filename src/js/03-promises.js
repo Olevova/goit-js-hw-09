db15a070aa9076d9ceb5b9105b5ded209c3cb719
Olevova <olevova1983@gmail.com>
2022-10-24 19:09:24 +0300

    third

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,20 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+const refs = {
+  delayEl: document.querySelector("input[name='delay']"),
+  stepDelayEl: document.querySelector("input[name='step']"),
+  amountEl: document.querySelector("input[name='amount']"),
+  btnEl: document.querySelector("button"),
 }
+console.log(refs);
+
+
+
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
