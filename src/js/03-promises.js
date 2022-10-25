aca88cc27bf2f8b114adcf075271f2c101798240
Olevova <olevova1983@gmail.com>
2022-10-25 20:32:12 +0300

    all3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -14,8 +14,8 @@ refs.btnEl.addEventListener("click", (event) => {
   const amount = Number(refs.amountEl.value);
   const firstDelay = Number(refs.delayEl.value);
   let delay = firstDelay;
-  for (let i = 0; i < amount; i += 1) {
-    createPromise(i, delay).then(r => console.log(r)).catch(e => console.log(e));
+  for (let position = 0; position < amount; position += 1) {
+    createPromise(position, delay).then(r => console.log(r)).catch(e => console.log(e));
     delay += Number(refs.stepDelayEl.value);
   }
 })
@@ -25,13 +25,14 @@ refs.btnEl.addEventListener("click", (event) => {
 // створюємо функція , яка в собі створює проміс
   function createPromise(position, delay) {
     const shouldResolve = Math.random() > 0.3;
-    // return promise = new Promise((resolve, reject) => {
+    return promise = new Promise((resolve, reject) => {
       setTimeout(() => {
         if (shouldResolve) {
-          Promise.resolve(Notiflix.Notify.success(`✅ Fulfilled promise ${position + 1} in ${delay}ms`));
+          resolve(Notiflix.Notify.success(`✅ Fulfilled promise ${position + 1} in ${delay}ms`));
         }
         else {
-          Promise.reject(Notiflix.Notify.failure(`❌ Rejected promise ${position + 1} in ${delay}ms`));
+          reject(Notiflix.Notify.failure(`❌ Rejected promise ${position + 1} in ${delay}ms`));
         }
       }, delay);
+    });
   };
