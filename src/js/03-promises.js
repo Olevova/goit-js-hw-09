150a36c14030c09b1fd375197ace9a0fb150249b
Olevova <olevova1983@gmail.com>
2022-10-25 18:52:37 +0300

    all

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,35 +1,37 @@
+import Notiflix from 'notiflix';
+
+// створюємо обєкт з елементами
 const refs = {
   delayEl: document.querySelector("input[name='delay']"),
   stepDelayEl: document.querySelector("input[name='step']"),
   amountEl: document.querySelector("input[name='amount']"),
   btnEl: document.querySelector("button"),
 }
-console.log(refs.delayEl.value);
-
+// console.log(refs.delayEl.value);
+// вішаємо слухача на кнопку для виклику промісів
 refs.btnEl.addEventListener("click", (event) => {
   event.preventDefault();
   const amount = Number(refs.amountEl.value);
   const firstDelay = Number(refs.delayEl.value);
     let delay = firstDelay;
   for (let i = 0; i < amount; i += 1) {
-    console.log(delay, amount, firstDelay);
     createPromise(i, delay).then(r => console.log(r)).catch(e => console.log(e));
     delay += Number(refs.stepDelayEl.value);
   }
 })
 
-// createPromise().then(r => console.log(r)).catch(e => console.log(e));
 
 
+// створюємо функція , яка в собі створює проміс
   function createPromise(position, delay) {
     const shouldResolve = Math.random() > 0.3;
     return promise = new Promise((resolve, reject) => {
       setTimeout(() => {
         if (shouldResolve) {
-          resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+          resolve(Notiflix.Notify.success(`✅ Fulfilled promise ${position + 1} in ${delay}ms`));
         }
         else {
-          reject(`❌ Rejected promise ${position} in ${delay}ms`);
+          reject(Notiflix.Notify.failure(`❌ Rejected promise ${position+1} in ${delay}ms`));
         }
       }, delay);
     });
